e82120b8004a996d4d737728dc1211ff2dc42518
LiZhigang <lzg2006bj@163.com>
2024-03-10 19:40:53 +0800

    + add a new project -- infrared-ray-ctrl

---
+++ examples/protocols/http_server/infrared-ray-ctrl/main/httpClient.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_tls.h"
+#include "esp_http_client.h"
+
+#define MAX_HTTP_OUTPUT_BUFFER  256
+#define HTTP_SRV_HOST = "smartonoff-f158.local"
+#define HTTP_SRV_PORT = 80
+
+static const char *HTTP_CLI_TAG = "HTTP_CLIENT";
+
+esp_err_t _httpEvtHandler(esp_http_client_event_t *evt) {
+  static char *output_buffer; // Buffer to store response of http request from event handler
+  static int output_len;      // Stores number of bytes read
+  switch (evt->event_id) {
+    case HTTP_EVENT_ERROR:
+      ESP_LOGD(HTTP_CLI_TAG, "HTTP_EVENT_ERROR");
+      break;
+    case HTTP_EVENT_ON_CONNECTED:
+      ESP_LOGD(HTTP_CLI_TAG, "HTTP_EVENT_ON_CONNECTED");
+      break;
+    case HTTP_EVENT_HEADER_SENT:
+      ESP_LOGD(HTTP_CLI_TAG, "HTTP_EVENT_HEADER_SENT");
+      break;
+    case HTTP_EVENT_ON_HEADER:
+      ESP_LOGD(HTTP_CLI_TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+      break;
+    case HTTP_EVENT_ON_DATA:
+      ESP_LOGD(HTTP_CLI_TAG, "HTTP_EVENT_ON_DATA, len=%d", evt->data_len);
+      /*
+      *  Check for chunked encoding is added as the URL for chunked encoding used in this example returns binary data.
+      *  However, event handler can also be used in case chunked encoding is used.
+      */
+      if (!esp_http_client_is_chunked_response(evt->client)) {
+        // If user_data buffer is configured, copy the response into the buffer
+        if (evt->user_data) {
+          memcpy((void*)(u32_t(evt->user_data) + output_len), evt->data, evt->data_len);
+        } else {
+          if (output_buffer == NULL) {
+            output_buffer = (char *)malloc(esp_http_client_get_content_length(evt->client));
+            output_len = 0;
+            if (output_buffer == NULL) {
+              ESP_LOGE(HTTP_CLI_TAG, "Failed to allocate memory for output buffer");
+              return ESP_FAIL;
+            }
+          }
+          memcpy(output_buffer + output_len, evt->data, evt->data_len);
+        }
+        output_len += evt->data_len;
+      }
+      break;
+    case HTTP_EVENT_ON_FINISH:
+      ESP_LOGD(HTTP_CLI_TAG, "HTTP_EVENT_ON_FINISH");
+      if (output_buffer != NULL)
+      {
+        // Response is accumulated in output_buffer. Uncomment the below line to print the accumulated response
+        // ESP_LOG_BUFFER_HEX(HTTP_CLI_TAG, output_buffer, output_len);
+        free(output_buffer);
+        output_buffer = NULL;
+      }
+      output_len = 0;
+      break;
+    case HTTP_EVENT_DISCONNECTED:
+      ESP_LOGI(HTTP_CLI_TAG, "HTTP_EVENT_DISCONNECTED");
+      int mbedtls_err = 0;
+      esp_err_t err = esp_tls_get_and_clear_last_error((esp_tls_error_handle_t)(evt->data), &mbedtls_err, NULL);
+      if (err != 0)
+      {
+        ESP_LOGI(HTTP_CLI_TAG, "Last esp error code: 0x%x", err);
+        ESP_LOGI(HTTP_CLI_TAG, "Last mbedtls failure: 0x%x", mbedtls_err);
+      }
+      if (output_buffer != NULL)
+      {
+        free(output_buffer);
+        output_buffer = NULL;
+      }
+      output_len = 0;
+      break;
+  }
+  return ESP_OK;
+}
+
+void sendHttpRequest(char* pStrQuery) { // pStrQuery: switch_on / switch_off
+  esp_http_client_config_t config = {};
+  config.host = HTTP_SRV_HOST,
+  config.port = HTTP_SRV_PORT,
+  config.method = HTTP_METHOD_GET;
+  config.path = "/";
+  config.query = pStrQuery;
+  config.transport_type = HTTP_TRANSPORT_OVER_TCP;
+  config.event_handler = _httpEvtHandler;
+  esp_http_client_handle_t client = esp_http_client_init(&config);
+  esp_err_t err = esp_http_client_open(client, 0);
+  if (err != ESP_OK) {
+    ESP_LOGE(HTTP_CLI_TAG, "Failed to open HTTP connection: %s", esp_err_to_name(err));
+  } else {
+    int content_length = esp_http_client_fetch_headers(client);
+    if (content_length < 0) {
+      ESP_LOGE(HTTP_CLI_TAG, "HTTP client fetch headers failed");
+    } else {
+      char output_buffer[MAX_HTTP_OUTPUT_BUFFER] = { 0 }; // Buffer to store response of http request
+      int data_read = esp_http_client_read_response(client, output_buffer, MAX_HTTP_OUTPUT_BUFFER);
+      if (data_read >= 0) {
+        ESP_LOGI(HTTP_CLI_TAG, "HTTP GET Status = %d, data_read = %d, content_length = %d",
+          esp_http_client_get_status_code(client), data_read, esp_http_client_get_content_length(client));
+        ESP_LOGI(HTTP_CLI_TAG, "HTTP GET output_buffer = %s", output_buffer);
+      } else {
+        ESP_LOGE(HTTP_CLI_TAG, "Failed to read response");
+      }
+    }
+  }
+  esp_http_client_cleanup(client);
+}
+
+#ifdef __cplusplus
+}
+#endif
---
+++ examples/protocols/http_server/infrared-ray-ctrl/main/passiveInfraredSensor.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+#define GPIO_INPUT_INFRARED_RAY GPIO_NUM_26 // input, pulled up, interrupt from rising edge
+#define GPIO_INPUT_PIN_SEL      (1ULL << GPIO_INPUT_INFRARED_RAY)
+#define ESP_INTR_FLAG_DEFAULT   0
+#define GPIO_LED_BLUE           GPIO_NUM_2 // esp32s on board blue LED
+#define LED_BLINK_INTERVAL      1000
+#define TASK_LOOP_PRIORITY      2
+#define G_X_QUEUE_LENGTH        16
+#define G_TASK_STACK_SIZE       (1024 * 2) // 2 KiB
+
+static const char* PIR_CTRL_TAG = "PIR_CTRL"; // PIR: passive infrared ( sensor )
+static QueueHandle_t G_X_QUEUE_OBJ = NULL;
+
+static void IRAM_ATTR gpio_isr_handler(void *arg) {
+  uint32_t gpio_num = (uint32_t)arg;
+  xQueueSendFromISR(G_X_QUEUE_OBJ, &gpio_num, NULL); // ISR: interrupt service routine
+}
+
+static void gpio_task_q_recv(void *arg) {
+  gpio_num_t io_num;
+  for (;;) {
+    if (xQueueReceive(G_X_QUEUE_OBJ, &io_num, portMAX_DELAY)) {
+      ESP_LOGI(PIR_CTRL_TAG, "GPIO[%d] interrupt, level: %d", io_num, gpio_get_level(io_num));
+      gpio_set_level(GPIO_LED_BLUE, 1); // on
+      vTaskDelay(LED_BLINK_INTERVAL / portTICK_PERIOD_MS);
+      gpio_set_level(GPIO_LED_BLUE, 0); // off
+    }
+  }
+}
+
+void initPIRSensorLoop() {
+  gpio_reset_pin(GPIO_LED_BLUE);
+  gpio_set_direction(GPIO_LED_BLUE, GPIO_MODE_OUTPUT);
+
+  gpio_config_t io_conf = {};                // zero-initialize the config structure.
+  io_conf.intr_type = GPIO_INTR_POSEDGE;     // 仅上升沿产生中断
+  io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL; // bit mask of the pins, use GPIO* here
+  io_conf.mode = GPIO_MODE_INPUT;            // set as input mode
+  io_conf.pull_up_en = GPIO_PULLUP_ENABLE;   // enable pull-up mode
+  gpio_config(&io_conf);
+
+  // create a queue to handle gpio event from isr
+  G_X_QUEUE_OBJ = xQueueCreate(G_X_QUEUE_LENGTH, sizeof(uint32_t));
+  // start gpio task
+  xTaskCreate(gpio_task_q_recv, "GPIO_TASK_Q_RECV", G_TASK_STACK_SIZE, NULL, tskIDLE_PRIORITY + TASK_LOOP_PRIORITY, NULL);
+
+  // install gpio isr service
+  gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+  // hook isr handler for specific gpio pin
+  gpio_isr_handler_add(GPIO_INPUT_INFRARED_RAY, gpio_isr_handler, (void *)GPIO_INPUT_INFRARED_RAY);
+
+  ESP_LOGI(PIR_CTRL_TAG, "[APP] Free memory: %ld bytes", esp_get_free_heap_size());
+}
+
+#ifdef __cplusplus
+}
+#endif
