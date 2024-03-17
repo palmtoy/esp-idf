ea8d90b9cc9e9ae9317cfd33566eb8e988524ade
LiZhigang <lzg2006bj@163.com>
2024-03-17 10:53:06 +0800

    * update some configs for esp32c3 in infrared-ray-ctrl.c

--- examples/peripherals/gpio/generic_gpio/main/infrared-ray-ctrl.c
+++ examples/peripherals/gpio/generic_gpio/main/infrared-ray-ctrl.c
@@ -7,13 +7,13 @@
 #include "driver/gpio.h"
 
 /**
- * GPIO26:  input, pulled up, interrupt from rising edge and/or falling edge.
+ * GPIO18:  input, pulled up, interrupt from rising edge and/or falling edge.
  */
-#define GPIO_INPUT_INFRARED_RAY 26
+#define GPIO_INPUT_INFRARED_RAY 18
 #define GPIO_INPUT_PIN_SEL 1ULL << GPIO_INPUT_INFRARED_RAY
 #define ESP_INTR_FLAG_DEFAULT 0
 
-#define GPIO_LED_BLUE 2  // ESP32S on board Blue LED
+#define GPIO_LED_BLUE 4  // esp32c3 on board green LED
 #define BLINK_INTERVAL 1000
 
 static QueueHandle_t gpio_evt_queue = NULL;
@@ -39,9 +39,9 @@ void app_main(void) {
   gpio_reset_pin(GPIO_LED_BLUE);
   gpio_set_direction(GPIO_LED_BLUE, GPIO_MODE_OUTPUT);
 
-  gpio_config_t io_conf = {}; // zero-initialize the config structure.
-  io_conf.intr_type = GPIO_INTR_POSEDGE;  // 仅上升沿产生中断
-  // io_conf.intr_type = GPIO_INTR_ANYEDGE;     // 上升沿 & 下降沿都产生中断
+  gpio_config_t io_conf = {};                // zero-initialize the config structure.
+  io_conf.intr_type = GPIO_INTR_POSEDGE;     // 仅上升沿产生中断
+  // io_conf.intr_type = GPIO_INTR_ANYEDGE;  // 上升沿 & 下降沿都产生中断
   io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL; // bit mask of the pins, use GPIO* here
   io_conf.mode = GPIO_MODE_INPUT;            // set as input mode
   io_conf.pull_up_en = 1;                    // enable pull-up mode
